28106607ec212c686cad522b72e6c9010492e4f7
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2017-12-22 11:26:46 +0000

    Project setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    compress: true,
+    port: 5000
+  }
+};
