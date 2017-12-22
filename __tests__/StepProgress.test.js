4fe26da597bfecb31e81da7aef18698a5589eced
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2017-12-22 19:14:08 +0000

    Add more logic

--- __tests__/StepProgress.test.js
+++ __tests__/StepProgress.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Step from 'components/Step';
+import Progress from 'components/Progress';
 import StepProgress from 'components/StepProgress';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 describe('StepProgress', () => {
   let renderedComponent;
@@ -11,8 +12,14 @@ describe('StepProgress', () => {
   };
 
   beforeAll(() => {
-    renderedComponent = shallow(<StepProgress {...props} />);
+    // Spy
+    StepProgress.prototype.onStepClick = jest.fn().mockImplementation(StepProgress.prototype.onStepClick);
+    renderedComponent = mount(<StepProgress {...props} />);
     renderedSteps = renderedComponent.find(Step);
+  });
+
+  beforeEach(() => {
+    StepProgress.prototype.onStepClick.mockClear();
   });
 
   it('should render steps', () => {
@@ -21,5 +28,42 @@ describe('StepProgress', () => {
 
   it('should pass the correct steps labels', () => {
     expect(renderedSteps.map((step) => step.props().label)).toEqual(expect.arrayContaining(props.stepsLabels))
+  });
+
+  it('should pass incremented IDs', () => {
+    expect(renderedSteps.map((step) => step.props().id)).toEqual(expect.arrayContaining([1,2,3]))
+  });
+
+  it('should set the correct steps as active', () => {
+    renderedComponent.setState({
+      activeStep: 2,
+    });
+
+    let renderedSteps = renderedComponent.find(Step);
+
+    expect(renderedSteps.at(0).props().isActive).toBe(true);
+    expect(renderedSteps.at(1).props().isActive).toBe(true);
+    expect(renderedSteps.at(2).props().isActive).toBe(false);
+  });
+
+  it('should render `Progress` component with the correct props', () => {
+    renderedComponent.setState({
+      activeStep: 2,
+    });
+
+    let progressComponentProps = renderedComponent.find(Progress).props();
+
+    expect(progressComponentProps.steps).toEqual(props.stepsLabels.length)
+    expect(progressComponentProps.activeStep).toEqual(2);
+  });
+
+  it('should call `onStepClick` method when a step is clicked passing its index as argument and set it to state', () => {
+    let secondStep = renderedSteps.at(1);
+
+    secondStep.find('div').simulate('click');
+
+    expect(StepProgress.prototype.onStepClick.mock.calls).toHaveLength(1);
+    expect(StepProgress.prototype.onStepClick.mock.calls[0][0]).toEqual(2);
+    expect(renderedComponent.state().activeStep).toEqual(2);
   });
 });
--- src/components/StepProgress.js
+++ src/components/StepProgress.js
@@ -2,16 +2,45 @@ import React from 'react';
 import Progress from './Progress';
 import Step from './Step';
 
-const StepProgress = (props) => {
-  return (
-    <div className="step-progress-wrapper">
-      <Progress steps={props.stepsLabels.length} activeStep={0} />
+class StepProgress extends React.Component {
+  constructor(props) {
+    super(props);
 
-      {props.stepsLabels.map((step, i) => (
-        <Step key={`${step}_${i}`} label={step} />
-      ))}
-    </div>
-  );
-};
+    this.onStepClick = this.onStepClick.bind(this);
+    this.state = {
+      activeStep: 0,
+    };
+  }
+
+  onStepClick(stepNumber) {
+    this.setState({
+      activeStep: stepNumber,
+    });
+  }
+
+  render() {
+    let { stepsLabels } = this.props;
+
+    return (
+      <div className="step-progress-wrapper">
+        <Progress steps={stepsLabels.length} activeStep={this.state.activeStep} />
+
+        {stepsLabels.map((step, i) => {
+          let uniqueId = i+1;
+
+          return (
+            <Step
+              key={`${step}_${i}`}
+              isActive={uniqueId <= this.state.activeStep}
+              id={uniqueId}
+              label={step}
+              onClick={this.onStepClick}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
 
 export default StepProgress;
